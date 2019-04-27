46f846b7522172daed7ec4686c922d434e9b8a0e
Toni Teofilovic <20360072+tteofilovic@users.noreply.github.com>
2019-04-27 07:28:11 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,33 +1,69 @@
 import React, {Component} from 'react';
 import BarChart from './components/BarChart';
+import _ from './../node_modules/lodash'
 import './App.css';
 
 
 class App extends Component {
 
-  constructor(){
-    super();
+  constructor(props){
+    super(props);
     const data = require('./components/data.json');
-    //this.state = data;
+    const source = _.cloneDeep(data);
+    this.state = {
+      original: data, 
+      source: source,
+      width:  700,
+      height: 500
+    };
+    this.filterResults = this.filterResults.bind(this)
+    this.resetResults = this.resetResults.bind(this)
+  }
 
-    // filter data, show last 10 data
-    let tempdata = data;
-    const keepEl = 3
+  filterResults(){
+    // filter data, show last 5 data
+    let tempdata = _.cloneDeep(this.state.original);
+    const keepEl = 5;
     tempdata.data.forEach(d => {
       let length = d.coordinates.length;
       d.coordinates = d.coordinates.filter(el => el.a >= length-keepEl)
     });
-    //console.log(this.state.data, tempdata)
-    
-    this.state = tempdata;
+    this.setState({
+      source: tempdata
+    });  
+  };
 
+  resetResults(){
+    this.setState({
+      source: _.cloneDeep(this.state.original)
+    });  
+  };
 
+  updateDimensions() {
+      let update_width  = window.innerWidth-100;
+      //let update_height = Math.round(update_width/4.4);
+      this.setState({ width: update_width/*, height: update_height*/ });
   }
 
-  render() {    
+  componentDidMount() {
+    this.updateDimensions();
+    window.addEventListener("resize", this.updateDimensions.bind(this));
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.updateDimensions.bind(this));
+  }
+
+  render() {  
     return (
       <div className="App">
-        <BarChart data={this.state.data} width={this.state.width} height={this.state.height} />
+        <button onClick={this.filterResults}>Show last 5 results</button>
+        <button onClick={this.resetResults}>Show All results</button>
+        <BarChart 
+          data={this.state.source.data} 
+          width={this.state.width} 
+          height={this.state.height} 
+        />
       </div>
     );
   }
