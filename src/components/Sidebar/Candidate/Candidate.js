b57f8a741d33570c958cfe07aee1c7eef1510212
Radoslav <zdravkovicradoslav@gmail.com>
2019-09-10 14:13:15 +0200

    WCS-2671_sidebar_styling_and_fonts_upload

--- src/components/Sidebar/Candidate/Candidate.js
+++ src/components/Sidebar/Candidate/Candidate.js
@@ -6,11 +6,12 @@ class Candidate extends Component {
             <div className="cand-view">
                 <div className="cand-main">
                     <div className="img-container">
+                        <img alt={this.props.name} src={require("../../../assets/images/candidates/" + this.props.name + ".jpg")}/>
                     </div>
                     <div className="cand-name">
                         {this.props.name}
                     </div>
-                    <div className="marker"></div>
+                    <div className="marker" style={{backgroundColor: this.props.color}} ></div>
                 </div>
             </div>
         );
--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,27 +1,84 @@
 import React, {Component} from 'react';
 import Candidate from './Candidate/Candidate';
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 class Sidebar extends Component {
+    constructor(props) {
+        super(props);
+        this.next = this.next.bind(this);
+        this.previous = this.previous.bind(this);
+        this.slide = this.slide.bind(this);
+    }
+
+    next() {
+        this.slider.slickNext();
+    }
+    previous() {
+        this.slider.slickPrev();
+    }
+    slide(y){
+        y > 0 ? (
+            this.slider.slickPrev()
+        ) : (
+            this.slider.slickNext()
+        )
+    }
+
     componentWillMount() {
+        window.addEventListener('wheel', (e) => {
+            this.slide(e.wheelDelta);
+        })
+
         const candidatesData = require('../../data/candidates.json');
 
         function getFullName(item) {
-            let fullname = [item.last_name];
+            let fullname = {fullName: item.last_name, color: item.color};
             return fullname;
         }
 
-        this.canidatesNames = candidatesData.candidates.map(getFullName);
+        this.canidatesData = candidatesData.candidates.map(getFullName);
     }
 
     render() {
-        let output = this.canidatesNames;
+        let output = this.canidatesData;
 
-        console.log(output);
+        var settings = {
+            vertical: true,
+            arrows: false,
+            speed: 500,
+            slidesToScroll: 5,
+            slidesToShow: 4,
+            infinite: true,
+            cssEase: "linear",
+            responsive: [
+                {
+                    breakpoint: 992,
+                    settings: {
+                        vertical: false,
+                        slidesToScroll: 5,
+                        slidesToShow: 20,
+                        infinite: true,
+                    },
+                },
+            ]
+        };
         return (
             <div className="graph-sidebar">
-                {output.map((candidateName) =>
-                    <Candidate name={candidateName} key={candidateName} />
+                <div className="cand-label">
+                    <h4 className="bold">SELECT YOUR CANDIDATES</h4>
+                </div>
+                <div className="cand-prev slider-arrow" onClick={this.previous}></div>
+                <Slider ref={c => (this.slider = c)} className="candidates" {...settings}>
+                {output.map((candidateData) =>
+                    <Candidate
+                        name={candidateData.fullName}
+                        color={candidateData.color}
+                        key={candidateData.fullName} />
                     )}
+                </Slider>
+                <div className="cand-next slider-arrow" onClick={this.next}></div>
             </div>
         );
     }
